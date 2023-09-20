b5c372570f0ba38492e9de762ed5cc81f48bbad0
Julian PR <j.pearbach@yahoo.com>
2023-09-20 15:54:33 -0700

    Started working on part 1.

--- proj3/code/julians_little_helpers.py
+++ proj3/code/julians_little_helpers.py
@@ -1,1 +1,35 @@
 # Julian's helpers functions for project 3
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def read_im(filepath):
+    if filepath[len(filepath)-3:] == 'tif':
+        return mpimg.imread(filepath)/65536.0 
+    if filepath[len(filepath)-3:] == 'jpg' or filepath[len(filepath)-4:] == 'jpeg':
+        return mpimg.imread(filepath)/255.0
+    else:
+        return mpimg.imread(filepath)
+
+def Display2Images(im1, im2, name1, name2):
+    # Set the desired width and height for the figure
+    fig_width = 12  # Width in inches
+    fig_height = 6  # Height in inches
+
+    # Create a figure with two subplots and set the figsize
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(fig_width, fig_height))
+
+    # Display image1 in the first subplot
+    ax1.imshow(im1)
+    ax1.set_title(name1)
+
+    # Display image2 in the second subplot
+    ax2.imshow(im2)
+    ax2.set_title(name2)
+
+    # Adjust spacing between subplots (optional)
+    plt.tight_layout()
+
+    # Show the figure
+    plt.show()
